f777a6cc86c2fc889808f4d378f63dbba3510787
Sypha999 <datonarinze@gmail.com>
2023-10-14 20:43:05 +0100

    to do list

--- src/main/java/core/nxg/serviceImpl/EmployerServiceImpl.java
+++ src/main/java/core/nxg/serviceImpl/EmployerServiceImpl.java
@@ -8,8 +8,11 @@ import core.nxg.exceptions.NotFoundException;
 import core.nxg.repository.EmployerRepository;
 import core.nxg.repository.UserRepository;
 import core.nxg.service.EmployerService;
+import core.nxg.utils.Helper;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Service;
 
 import java.security.Principal;
@@ -18,29 +21,36 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+//TODO use map to dto properly
 public class EmployerServiceImpl implements EmployerService {
 
     private final EmployerRepository employerRepository;
     private final UserRepository userRepository;
+    private final Helper helper;
     // private JavaLangInvokeAccess principal;
 
     @Override
     public List<EmployerDto> getAllEmployers() {
+
+        //TODO return pageable
         return employerRepository.findAll()
                 .stream().map(this::mapToDto).collect(Collectors.toList());
     }
 
-   // @Override
-    //public Employer createEmployer(EmployerDto employerDto) {
 
+    //TODO refactor to use HttpServletRequest
         @Override
-        public Employer createEmployer (EmployerDto employerDto, Principal principal){
+        public Employer createEmployer (EmployerDto employerDto, Principal principal /*HttpServletRequest request*/){
             String loggedInUserEmail = principal.getName();
+
+            //User user = helper.extractLoggedInUser(request);
 
             User user = userRepository.findByEmail(loggedInUserEmail)
                     .orElseThrow(() -> new NotFoundException("User with email " + loggedInUserEmail + " not found"));
 
             Employer employer = new Employer();
+            //TODO refactor code
+            //helper.copyFromDto(employerDto,employer);
             employer.setCompanyName(employerDto.getCompanyName());
             employer.setCompanyDescription(employerDto.getCompanyDescription());
             employer.setPosition(employerDto.getPosition());
@@ -88,6 +98,8 @@ public class EmployerServiceImpl implements EmployerService {
         return employerDto;
     }
 
+
+    //TODO return a response
     @Override
     public void deleteEmployer(Long employerId) {
         Employer employer = employerRepository.findById(employerId)
--- src/main/java/core/nxg/utils/Helper.java
+++ src/main/java/core/nxg/utils/Helper.java
@@ -1,4 +1,29 @@
 package core.nxg.utils;
 
-public class Helper {
+import core.nxg.configs.JwtService;
+import core.nxg.entity.User;
+import core.nxg.exceptions.NotFoundException;
+import core.nxg.repository.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class Helper<K,V> {
+    private final JwtService jwtService;
+    private final UserRepository userRepo;
+
+    public User extractLoggedInUser(HttpServletRequest request){
+        final String authHeader = request.getHeader("Authorization");
+        String jwt = authHeader.substring(7);
+        String email = jwtService.extractUsername(jwt);
+        return userRepo.findByEmail(email).orElseThrow(()-> new NotFoundException("User not found"));
+    }
+
+    public K copyFromDto ( V v,K k){
+        BeanUtils.copyProperties(v,k);
+        return k;
+    }
 }
