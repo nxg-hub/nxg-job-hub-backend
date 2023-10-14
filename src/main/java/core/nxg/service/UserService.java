6a8c825154163ea97d109404e5af7615468f0d6c
Sypha999 <datonarinze@gmail.com>
2023-10-14 20:43:05 +0100

    to do list

--- src/main/java/core/nxg/service/UserService.java
+++ src/main/java/core/nxg/service/UserService.java
@@ -1,16 +1,16 @@
-package core.nxg.service;
-
-import org.springframework.data.domain.Page;
-import org.springframework.stereotype.Service;
-
-import core.nxg.dto.LoginDTO;
-import core.nxg.dto.UserDTO;
-import core.nxg.entity.User;
-import org.springframework.data.domain.Pageable;
-
-@Service
-public interface  UserService<T> {
-    String createUser(UserDTO userDto) throws Exception; //Should return UserDto instead of user
-    Page<User> getAllUsers(Pageable pageable); //Should be a pageable and also return Pageable of userdto
-    String login(LoginDTO loginDTO ) throws Exception;
-}
+//package core.nxg.service;
+//
+//import org.springframework.data.domain.Page;
+//import org.springframework.stereotype.Service;
+//
+//import core.nxg.dto.LoginDTO;
+//import core.nxg.dto.UserDTO;
+//import core.nxg.entity.User;
+//import org.springframework.data.domain.Pageable;
+//
+//@Service
+//public interface  UserService<T> {
+//    String createUser(UserDTO userDto) throws Exception; //Should return UserDto instead of user
+//    Page<User> getAllUsers(Pageable pageable); //Should be a pageable and also return Pageable of userdto
+//    String login(LoginDTO loginDTO ) throws Exception;
+//}
