d51f38b230e5265d6dd13e285c6d772ce2dd1dcc
timibacco <foxxelit9@gmail.com>
2023-10-20 03:31:08 +0100

    On it.

--- src/main/java/core/nxg/NxgApplication.java
+++ src/main/java/core/nxg/NxgApplication.java
@@ -3,13 +3,7 @@ package core.nxg;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
-
-import java.util.Properties;
 
 @SpringBootApplication
 @ComponentScan("core.nxg")
@@ -21,23 +15,6 @@ public class NxgApplication {
 	}
 
 
-//	@Configuration
-//	public static class MailConfiguration {
-//
-//
-//		 @Bean
-//		 public JavaMailSender javaMailSender() {
-//		     JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-//		     mailSender.setHost("smtp.gmail.com");
-//		     mailSender.setPort(587);
-//		     mailSender.setUsername("");
-//		     mailSender.setPassword("");
-//		     Properties props = mailSender.getJavaMailProperties();
-//		     props.put("mail.transport.protocol", "smtp");
-//		     props.put("mail.smtp.auth", "true");
-//		     props.put("mail.smtp.starttls.enable", "true");
-//		     return mailSender;
-//		 }
-//	}
+
 }
 
