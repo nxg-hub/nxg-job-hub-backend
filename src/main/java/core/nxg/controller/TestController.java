51a7a34f25d3ac956c2a05840cfe76497688752e
Sypha999 <datonarinze@gmail.com>
2023-10-25 14:57:23 +0100

    added heroku deployment details

---
+++ src/main/java/core/nxg/controller/TestController.java
@@ -0,0 +1,15 @@
+package core.nxg.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+public class TestController {
+
+    @GetMapping("status")
+    public String getStatus(){
+        return "Job hub is up and running, time: "+ LocalDateTime.now();
+    }
+}
