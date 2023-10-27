7bd7172751414c3c41391da6126a1fcf4ba7f1ee
Oragbakosi Anthony Arinze <38960714+sypha999@users.noreply.github.com>
2023-10-27 12:39:05 +0100

    Unsecured all endpoints

--- src/main/java/core/nxg/utils/Endpoints.java
+++ src/main/java/core/nxg/utils/Endpoints.java
@@ -9,7 +9,8 @@ public final class Endpoints {
             "/swagger","/swagger-ui/**",
             "/v3/**",
             "/api/v1/auth/tech-talent/**",
-            "/api/v1/auth/**"
+            "/api/v1/auth/**",
+        "/**"
             
             
     };
