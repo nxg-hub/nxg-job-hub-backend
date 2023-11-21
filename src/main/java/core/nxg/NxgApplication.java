4209dac3b3619d8c597146423a2a5398ed3ec849
josephnma <josgolf@yahoo.ca>
2023-11-21 11:21:54 +0100

    adjusted db schema to update.

--- src/main/java/core/nxg/NxgApplication.java
+++ src/main/java/core/nxg/NxgApplication.java
@@ -2,14 +2,10 @@ package core.nxg;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("core.nxg")
-@EnableAutoConfiguration
 public class NxgApplication {
 
 	public static void main(String[] args) {
