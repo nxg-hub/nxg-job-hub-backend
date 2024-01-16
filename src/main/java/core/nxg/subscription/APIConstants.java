685591610e9fe79329407b42d68315529389288e
timibacco <foxxelit9@gmail.com>
2024-01-16 17:14:34 +0100

    subscription-createaccnt

--- src/main/java/core/nxg/subscription/APIConstants.java
+++ src/main/java/core/nxg/subscription/APIConstants.java
@@ -3,38 +3,28 @@ package core.nxg.subscription;
 public class APIConstants {
 
     public static final String BASE_URL = "https://api.paystack.co";
-    public static final String PAYSTACK_INIT_TRANSACTIONS = BASE_URL + "/transaction/initialize";
-    public static final String PAYSTACK_VERIFY_TRANSACTIONS = BASE_URL + "/transaction/verify/";
-    public static final String PAYSTACK_CHARGE_AUTHORIZATION = BASE_URL + "/transaction/charge_authorization";
-
-    //URL definitions for customer endpoint
-    public static final String PAYSTACK_CUSTOMER_URL = BASE_URL + "/customer";
-
-    //URL definitions for transaction endpoints
-    public static final String PAYSTACK_TRANSACTIONS_INITIALIZE_TRANSACTION = BASE_URL + "/transaction/initialize";
-    public static final String PAYSTACK_TRANSACTIONS_VERIFY_TRANSACTION = BASE_URL + "/transaction/verify/";
-    public static final String PAYSTACK_TRANSACTIONS_LIST_TRANSACTIONS = BASE_URL + "/transaction";
-    public static final String PAYSTACK_TRANSACTIONS_CHARGE_TOKEN = BASE_URL + "/transaction/charge_token";
-    public static final String PAYSTACK_TRANSACTIONS_EXPORT_TRANSACTIONS = BASE_URL + "/transaction/export";
-
-    //URL definitions for plan endpoint
-    public static final String PAYSTACK_PLANS_CREATE_PLAN = BASE_URL + "/plan";
-    public static final String PAYSTACK_PLANS_LIST_PLANS = BASE_URL + "/plan";
-    public static final String PAYSTACK_PLANS_FETCH_PLAN = BASE_URL + "/plan/";
-    public static final String PAYSTACK_PLANS_UPDATE_PLAN = BASE_URL + "/plan/";
-
-    //URL definitions for subscription endpoints
-    public static final String PAYSTACK_SUBSCRIPTIONS_CREATE_SUBSCRIPTION = BASE_URL + "/subscription";
-    public static final String PAYSTACK_SUBSCRIPTIONS_DISABLE_SUBSCRIPTION = BASE_URL + "/subscription/disable";
-    public static final String PAYSTACK_SUBSCRIPTIONS_ENABLE_SUBSCRIPTION = BASE_URL + "/subscription/enable";
-    public static final String PAYSTACK_SUBSCRIPTIONS_FETCH_SUBSCRIPTION = BASE_URL + "/subscription/";
-
-    //URL definitions for page endpoint
-    public static final String PAYSTACK_PAGES_CREATE_PAGE = BASE_URL + "/page";
-    public static final String PAYSTACK_PAGES_LIST_PAGES = BASE_URL + "/page";
-    public static final String PAYSTACK_PAGES_FETCH_PAGE = BASE_URL + "/page/";
-    public static final String PAYSTACK_PAGES_UPDATE_PAGE = BASE_URL + "/page/";
-
-
-
+    public static final String CUSTOMER_URL = BASE_URL + "/customer";
+    public static final String CUSTOMER_CREATE_URL = CUSTOMER_URL + "/create";
+    public static final String CUSTOMER_LIST_URL = CUSTOMER_URL + "/list";
+    public static final String CUSTOMER_FETCH_URL = CUSTOMER_URL + "/{id}";
+    public static final String CUSTOMER_UPDATE_URL = CUSTOMER_URL + "/{id}";
+    public static final String CUSTOMER_SET_RISK_ACTION_URL = CUSTOMER_URL + "/set_risk_action";
+    public static final String CUSTOMER_DEACTIVATE_AUTH_URL = CUSTOMER_URL + "/deactivate_authorization";
+    public static final String CUSTOMER_BLACKLIST_URL = CUSTOMER_URL + "/blacklist";
+    public static final String CUSTOMER_WHITELIST_URL = CUSTOMER_URL + "/whitelist";
+    public static final String CUSTOMER_DEACTIVATE_URL = CUSTOMER_URL + "/deactivate";
+    public static final String CUSTOMER_REACTIVATE_URL = CUSTOMER_URL + "/reactivate";
+    public static final String CUSTOMER_EXPORT_URL = CUSTOMER_URL + "/export";
+    public static final String CUSTOMER_MIGRATE_URL = CUSTOMER_URL + "/migrate";
+    public static final String CUSTOMER_SET_RISK_LEVEL_URL = CUSTOMER_URL + "/set_risk_level";
+    public static final String CUSTOMER_SET_LIMIT_URL = CUSTOMER_URL + "/set_limit";
+    public static final String CUSTOMER_ADD_INVOICE_LIMIT_URL = CUSTOMER_URL + "/add_invoice_limit";
+    public static final String CUSTOMER_REMOVE_INVOICE_LIMIT_URL = CUSTOMER_URL + "/remove_invoice_limit";
+    public static final String CUSTOMER_ADD_SUBSCRIPTION_LIMIT_URL = CUSTOMER_URL + "/add_subscription_limit";
+    public static final String CUSTOMER_REMOVE_SUBSCRIPTION_LIMIT_URL = CUSTOMER_URL + "/remove_subscription_limit";
+    public static final String CUSTOMER_ADD_AUTHORIZATION_URL = CUSTOMER_URL + "/add_authorization";
+    public static final String CUSTOMER_REMOVE_AUTHORIZATION_URL = CUSTOMER_URL + "/remove_authorization";
+    public static final String CUSTOMER_ADD_BLACKLIST_URL = CUSTOMER_URL + "/add_blacklist";
+    public static final String CUSTOMER_REMOVE_BLACKLIST_URL = CUSTOMER_URL + "/remove_blacklist";
+    public static final String CUSTOMER_ADD_WHITELIST_URL = CUSTOMER_URL + "/add_white_list";
 }
--- src/main/java/core/nxg/subscription/PayemntController.java
+++ src/main/java/core/nxg/subscription/PayemntController.java
@@ -1,27 +1,22 @@
 package core.nxg.subscription;
 
 
-
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nimbusds.jose.shaded.gson.JsonObject;
 import core.nxg.subscription.dto.CustomerDTO;
-import core.nxg.subscription.dto.SubscribeDTO;
-import core.nxg.subscription.dto.TransactionDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Map;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Slf4j
 @Controller
@@ -31,6 +26,7 @@ public class PayemntController {
 
     @Autowired
     private final SubscriptionService paymentService;
+
 
 
     @Operation(summary = "Create a subscription account for a user")
@@ -43,59 +39,12 @@ public class PayemntController {
     public ResponseEntity<JsonNode> createAccount(@RequestBody CustomerDTO customerdto) {
 
         try {
-            return ResponseEntity.status(HttpStatus.CREATED).body(paymentService.createSubscriber(customerdto));
+            return ResponseEntity.ok().body(paymentService.createSubscriber(customerdto));
 
 
         } catch (Exception e) {
             log.error("Could not create account for  " + customerdto.getEmail(), e);
-            ObjectMapper mapper = new ObjectMapper();
-            JsonNode err = mapper.convertValue(e.getMessage(), JsonNode.class);
-            return ResponseEntity.badRequest().body(err);
+            return ResponseEntity.badRequest().body(null);
         }
     }
-
-
-    @PostMapping("/initialize-transaction")
-    public ResponseEntity<JsonNode> intializeTransaction(@RequestBody TransactionDTO dto) throws Exception{
-
-        try {
-           JsonNode response = paymentService.
-                    initializeTransaction(dto);
-
-            return ResponseEntity.ok().body(response);
-        } catch (Exception e) {
-            log.error("Could not initialize transaction for  " + dto.getEmail(), e);
-            ObjectMapper mapper = new ObjectMapper();
-            JsonNode error = mapper.convertValue(e.getMessage(), JsonNode.class);
-
-            return ResponseEntity.badRequest().body(error);
-        }
-
-
-    }
-
-
-    @Operation(summary = "Subscribe a user to a plan. Based on the plan type, the user will be charged accordingly")
-    @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Subscribe a user with " +
-            "email address, planType(PLATINUM, GOLD, SILVER, FREE ) and a callback_url", required = true, content =
-    @Content(mediaType = "application/json",
-            schema = @Schema(implementation = SubscribeDTO.class)))
-    @PostMapping("/subscribe")
-    public ResponseEntity<JsonNode> subscribe(@RequestBody SubscribeDTO dto) throws Exception{
-
-        try {
-            JsonNode response = paymentService.subscribe(dto);
-
-            return ResponseEntity.ok().body(response);
-        } catch (Exception e) {
-            log.error("Could not initialize subscription for  " + dto.getEmail(), e);
-            ObjectMapper mapper = new ObjectMapper();
-            JsonNode error = mapper.convertValue(e.getMessage(), JsonNode.class);
-
-            return ResponseEntity.badRequest().body(error);
-        }
-
-
-    }
-
 }
