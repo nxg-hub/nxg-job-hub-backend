4e44c38f6b1d6b9ed1da7ebf131d29c3993c0cab
timibacco <foxxelit9@gmail.com>
2024-01-18 11:23:07 +0100

    intialze-transxn

--- src/main/java/core/nxg/subscription/SubscriptionService.java
+++ src/main/java/core/nxg/subscription/SubscriptionService.java
@@ -33,7 +33,7 @@ public class SubscriptionService {
     private final SubscriptionRepository subscriptionRepo;
 
     @Value("${paystack.secret.active}")
-    private String API_SECRET_KEY;
+    private String API_KEY;
 
 
     public JsonNode createSubscriber(CustomerDTO customerdto) throws Exception {
@@ -44,28 +44,29 @@ public class SubscriptionService {
 
             RestTemplate restTemplate = new RestTemplate();
             HttpHeaders headers = new HttpHeaders();
-            headers.set("Authorization", "Bearer " + API_SECRET_KEY);
+            headers.set("Authorization", "Bearer " + API_KEY);
             HttpEntity<CustomerDTO> request = new HttpEntity<>(customerdto, headers);
 
 
             ResponseEntity<JsonNode> response = restTemplate.exchange(
-                    APIConstants.CUSTOMER_URL,
+                    APIConstants.PAYSTACK_CUSTOMER_URL,
                     HttpMethod.POST,
                     request,
                     JsonNode.class);
 
-            if (response.hasBody() && response.getBody().get("status").toString().equals("true")) {
+            if (response.hasBody() && response.getBody().get("status").booleanValue()) {
 
                     Subscriber subscriber = new Subscriber();
                     subscriber.setCustomerId(response.getBody().get("data").get("customer_code").toString());
                     subscriber.setEmail(response.getBody().get("data").get("email").toString());
+                    subscriber.setPlanType(customerdto.getPlanType());
                     subscriber.setUser(user);
                     userRepo.save(user);
                     subscriptionRepo.saveAndFlush(subscriber);
-                return response.getBody();
+                    return response.getBody();
 
             } else {
-
+                Logger.getLogger(SubscriptionService.class.getName()).log(Level.SEVERE, "Could not create account for " + customerdto.getEmail());
                 throw new Exception("Could not create account for " + customerdto.getEmail());
 
             }
@@ -78,10 +79,48 @@ public class SubscriptionService {
 
 
 
+    public JsonNode chargeSubscriber(PlanType planType, String reference, String amount,  // charge the subscriber based on the plan
+                                     String email,
+                                     String plan, String callback_url){
 
 
-//    public void createSubscription(String customer, String plan, String authorization){
-//        Subscriptions subscriptions = new Subscriptions();
-//        subscriptions.createSubscription(customer, plan, authorization);
-//    }
+        if (planType.equals(PlanType.GOLD))
+            amount = "50";
+        else if (planType.equals(PlanType.SILVER)) {
+
+            amount = "20";
+        }
+        else
+        init(reference,
+                amount,
+                email,
+                plan,
+                callback_url);
+
+        return null;
+    }
+
+    private void init(String reference, String amount, String email,  // initialize a transaction first.
+                      String plan, String callback_url){
+        Map<String, Object> content = new HashMap<>();
+        content.put("reference", reference);
+        content.put("amount", amount);
+        content.put("email", email);
+        content.put("plan", plan) ; // todo: make "plan" set implicitly by server. not from request to server.
+        content.put("callback_url", callback_url );
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + API_KEY);
+        HttpEntity<Map<String,Object>> request = new HttpEntity<>(content,headers);
+
+        ResponseEntity<JsonNode> response = restTemplate.exchange(
+                APIConstants.PAYSTACK_INIT_TRANSACTIONS,
+                HttpMethod.POST,
+                request,
+                JsonNode.class
+        );
+
+
+    }
+
 }
