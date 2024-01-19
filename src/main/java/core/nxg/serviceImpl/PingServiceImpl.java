7f731fd7e74d09d045cb735bc0158dac2c661acf
josephnma <josgolf@yahoo.ca>
2024-01-19 10:53:11 +0100

    ping render to fix spin down

---
+++ src/main/java/core/nxg/serviceImpl/PingServiceImpl.java
@@ -0,0 +1,23 @@
+package core.nxg.serviceImpl;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class PingServiceImpl  {
+
+    @Value("${server.url}")
+    private String serverUrl;  // Use your server URL here
+    @Scheduled(fixedRate = 1000000)  // 10 minutes in milliseconds
+    public void pingServer() {
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+            restTemplate.getForObject(serverUrl, String.class);
+            System.out.println("Server pinged at " + System.currentTimeMillis());
+        } catch (Exception e) {
+            System.err.println("Error while pinging the server: " + e.getMessage());
+        }
+    }
+}
