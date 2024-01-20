0e6925bbd6cd09c355ea8594d925a89ddc83e5fd
timibacco <foxxelit9@gmail.com>
2024-01-20 10:46:19 +0100

    api-service

--- src/main/java/core/nxg/subscription/SubscriptionService.java
+++ src/main/java/core/nxg/subscription/SubscriptionService.java
@@ -6,11 +6,10 @@ import core.nxg.entity.User;
 import core.nxg.exceptions.UserNotFoundException;
 import core.nxg.repository.UserRepository;
 import core.nxg.subscription.dto.CustomerDTO;
+import core.nxg.subscription.dto.TransactionDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
+
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -32,27 +31,21 @@ public class SubscriptionService {
     @Autowired
     private final SubscriptionRepository subscriptionRepo;
 
-    @Value("${paystack.secret.active}")
-    private String API_KEY;
+    @Autowired
+    private final APIService apiService;
 
 
-    public JsonNode createSubscriber(CustomerDTO customerdto) throws HttpClientErrorException, JsonProcessingException{
+
+
+
+    public JsonNode createSubscriber(CustomerDTO customerdto) throws HttpClientErrorException, JsonProcessingException {
         try {
             User user = userRepo.findByEmail(customerdto.getEmail()).
                     orElseThrow(() -> new UserNotFoundException("User not found! Please register first"));
 
 
-            RestTemplate restTemplate = new RestTemplate();
-            HttpHeaders headers = new HttpHeaders();
-            headers.set("Authorization", "Bearer " + API_KEY);
-            HttpEntity<CustomerDTO> request = new HttpEntity<>(customerdto, headers);
 
-
-            ResponseEntity<JsonNode> response = restTemplate.exchange(
-                    APIConstants.PAYSTACK_CUSTOMER_URL,
-                    HttpMethod.POST,
-                    request,
-                    JsonNode.class);
+            ResponseEntity<JsonNode> response = apiService.createCustomer(customerdto);
 
             if (response.hasBody() && response.getBody().get("status").booleanValue()) {
 
@@ -82,78 +75,26 @@ public class SubscriptionService {
     }
 
 
-//    public JsonNode chargeSubscriber(PlanType planType, String reference, String amount,  // charge the subscriber based on the plan
-//                                     String email,
-//                                     String plan, String callback_url) throws JsonProcessingException {
-//
-//
-//        if (planType.equals(PlanType.GOLD))
-//            amount = "50";
-//        else if (planType.equals(PlanType.SILVER)) {
-//
-//            amount = "20";
-//        }
-//        ;
-//        JsonNode response = initialize(reference,
-//                amount,
-//                email,
-//                plan,
-//                callback_url);
-//
-//        if (response.get("status").asBoolean(true))
-//
-//            return response;
-//
-//        return null;
-//    }
-    
 
 
-    public JsonNode verifyTransaction(String reference) throws JsonProcessingException {
-        RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", "Bearer " + API_KEY);
-        HttpEntity<Map<String,Object>> request = new HttpEntity<>(headers);
 
-        ResponseEntity<JsonNode> response = restTemplate.exchange(
-                APIConstants.PAYSTACK_VERIFY_TRANSACTIONS + reference,
-                HttpMethod.GET,
-                request,
-                JsonNode.class);
+    public JsonNode initializeTransaction(TransactionDTO dto) throws JsonProcessingException, HttpClientErrorException{
+        try{
 
-        if (response.hasBody() && response.getBody().get("status").booleanValue()) {
-            return response.getBody();
-        } else {
+            return apiService.initialize(dto);}
+        catch (Exception e){
             Logger.getLogger(SubscriptionService.class.getName())
                     .log(
-                            Level.SEVERE, "Could not verify transaction for " + reference);
-            throw new RuntimeException("Could not verify transaction for " + reference);
+                            Level.SEVERE, "Could not initialize transaction for " + dto.getEmail());
+            throw new RuntimeException("Could not initialize transaction for " + dto.getEmail());
+
         }
     }
-    public JsonNode initializeTransaction(String reference, String amount, String email,  // initialize a transaction .
-                      String plan, String callback_url) throws  JsonProcessingException, HttpClientErrorException {
-
-        reference = "jobhub" + System.currentTimeMillis();
-        Map<String, Object> content = new HashMap<>();
-        content.put("reference", reference);
-        content.put("amount", amount);
-        content.put("email", email);
-        content.put("plan", plan) ; // todo: make "plan" set implicitly by server. not from request to server.
-        content.put("callback_url", "subscription/verify/"+reference );
-        RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", "Bearer " + API_KEY);
-        HttpEntity<Map<String,Object>> request = new HttpEntity<>(content,headers);
-
-        ResponseEntity<JsonNode> response = restTemplate.exchange(
-                APIConstants.PAYSTACK_INIT_TRANSACTIONS,
-                HttpMethod.POST,
-                request,
-                JsonNode.class);
-
-                return response.getBody();
 
 
-    }
+
+
+
+
 
 }
