db9f1f89d0c6e380802b836744f05e6e3e860dc4
timibacco <foxxelit9@gmail.com>
2024-01-21 13:11:51 +0100

    extensive-error-handling

--- src/main/java/core/nxg/controller/TechTalentController.java
+++ src/main/java/core/nxg/controller/TechTalentController.java
@@ -13,6 +13,7 @@ import io.swagger.v3.oas.annotations.Operation;
 
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
+
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
