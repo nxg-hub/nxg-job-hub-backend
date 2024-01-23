bd7addeb732afc213176333bb09c0797980b894f
timibacco <foxxelit9@gmail.com>
2024-01-23 03:37:57 +0100

    testing-remote

--- src/main/java/core/nxg/subscription/PayemntController.java
+++ src/main/java/core/nxg/subscription/PayemntController.java
@@ -20,11 +20,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @Slf4j
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/subscriptions")
 public class PayemntController {
