2852f8830db47b89bb6752464882ce4f192aabd4
josephnma <josgolf@yahoo.ca>
2024-01-25 20:20:52 +0100

    bug fix

--- src/main/java/core/nxg/serviceImpl/PingServiceImpl.java
+++ src/main/java/core/nxg/serviceImpl/PingServiceImpl.java
@@ -5,6 +5,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+
 @Service
 public class PingServiceImpl  {
 
