cb0835fb132874ff48c4576365eaa7d039a9d502
timibacco <foxxelit9@gmail.com>
2024-03-04 19:26:45 +0100

    event-parsing

--- src/main/java/core/nxg/subscription/service/APIService.java
+++ src/main/java/core/nxg/subscription/service/APIService.java
@@ -5,6 +5,10 @@ import com.fasterxml.jackson.databind.JsonNode;
 import core.nxg.subscription.enums.APIConstants;
 import core.nxg.subscription.dto.CustomerDTO;
 import core.nxg.subscription.dto.TransactionDTO;
+import core.nxg.subscription.enums.EventType;
+import core.nxg.subscription.enums.SubscriptionStatus;
+import core.nxg.subscription.repository.SubscriptionRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -13,18 +17,24 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 
+import static core.nxg.subscription.enums.EventType.*;
 
 
 
+@Slf4j
 @Service
 public class APIService {
 
 
     @Value("${paystack.secret.active}")
     private String API_KEY;
+
+
+    private SubscriptionRepository repo;
 
     public ResponseEntity<JsonNode> createCustomer(CustomerDTO dto) throws JsonProcessingException, HttpClientErrorException {
 
@@ -92,6 +102,54 @@ public class APIService {
     public JsonNode validateIdentity(Map<String,Object> body, String customer_code){
         return post(body,
                 APIConstants.PAYSTACK_CUSTOMER_URL+ "/" + customer_code + "/identification" ).getBody();
+
+    }
+
+
+    public void parseEvents(EventType event, String email) {
+        String eventString = event.getEvent();
+        System.out.println("Event: " + event);
+        switch(eventString){
+            case "subscription.create":
+               log.info("Subscription event received: {}", event.getEvent());
+               repo.findByEmail(email).ifPresent(subscriber -> {
+                subscriber.setSubscriptionStatus(SubscriptionStatus.ACTIVE);
+                subscriber.setSubscriptionStarts(LocalDate.now());
+                repo.save(subscriber);});
+                break;
+                case "charge.failed":
+
+                log.info("A Charge event received {}", eventString);
+                break;
+            case "charge.success":
+                log.info("A Charge event created {}", eventString);
+                break;
+            case "subscription.disable":
+                System.out.println("Subscription disabled");
+                break;
+            case "invoice.create":
+                System.out.println("Invoice created");
+                break;
+            case "invoice.update":
+                System.out.println("Invoice updated");
+                break;
+            case "invoice.success":
+                System.out.println("Invoice payment failed");
+                break;
+            case "transfer.success":
+                System.out.println("Transfer successful");
+                break;
+            case "transfer.failed":
+                System.out.println("Transfer failed");
+                break;
+            case "transfer.reversed":
+                System.out.println("Transfer reversed");
+                break;
+            default:
+                log.info("An unrecognized event was received: {}", eventString);
+                break;
+        }
+
 
     }
 }
