80880bf229ed5ccae6d98c5fa4ce09012a8383a2
Joseph Nma <josgolf@yahoo.ca>
2024-07-03 10:52:39 +0100

    swagger config for token

---
+++ src/main/java/core/nxg/configs/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package core.nxg.configs;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "API Documentation", version = "1.0.0", description = "API documentation with Bearer Token Authentication"))
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("bearer-key",
+                        new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList("bearer-key"));
+    }
+}
